dd686f2b2d747288ee58e8b5b693cdf222ad1d4a
Misha <mishachernyshov2@gmail.com>
2021-03-18 20:11:06 +0200

    Models were added

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+api_models = [
+    models.AutomaticMachineDish,
+    models.AutomaticMachineType,
+    models.CateringEstablishment,
+    models.CateringEstablishmentAutomaticMachine,
+    models.CateringEstablishmentDish,
+    models.Dish,
+    models.DishIngredient,
+    models.DishReport,
+    models.Ingredient,
+    models.OrderedDishIngredient,
+    models.User,
+]
+
+admin.site.register(api_models)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class CateringEstablishment(models.Model):
+    establishment_code = models.UUIDField()
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(blank=True, upload_to='dishImages/')
+    description = models.TextField()
+    type = models.TextField(max_length=20)
+    popularity = models.CharField(max_length=20)
+    rate = models.IntegerField()
+
+
+class CateringEstablishmentDish(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    catering_establishment = models.ForeignKey(CateringEstablishment,
+                                               on_delete=models.CASCADE)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.FloatField()
+
+
+class DishIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    status = models.BooleanField()
+
+
+class AutomaticMachineType(models.Model):
+    pass
+
+
+class CateringEstablishmentAutomaticMachine(models.Model):
+    catering_establishment = models.ForeignKey(CateringEstablishment,
+                                               on_delete=models.CASCADE)
+    automatic_machine_type = models.ForeignKey(AutomaticMachineType,
+                                               on_delete=models.CASCADE)
+
+
+class AutomaticMachineDish(models.Model):
+    automatic_machine_type = models.ForeignKey(AutomaticMachineType,
+                                               on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+
+
+class User(get_user_model()):
+    status = models.CharField(max_length=20)
+
+
+class DishReport(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    text = models.TextField()
+    publication_date = models.DateField()
+
+
+class OrderedDishIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    weight_or_volume = models.FloatField()
+    order_date = models.DateField()
+    order_time = models.TimeField()
